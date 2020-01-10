9b227d40458765c2e260a3783927b27f8ab3de9a
Nelson Knight <nelson.knight92@gmail.com>
2020-01-10 10:48:26 -0700

    Started over with a new repo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Mongo-News
+
+## Overview
+
+
+In this assignment, you'll create a web app that lets users view and leave comments on the latest news. But you're not going to actually write any articles; instead, you'll flex your Mongoose and Cheerio muscles to scrape news from another site.
+
+### Click Scrape NYT to get the news.
+
+<img width="1639" alt="Readme md image" src="public/assets/images/readme.png">
+
+### If you like an article you can SAVE ARTICLE to keep it with other saved articles.
+
+<img width="1639" alt="Readme md image" src="public/assets/images/readme-2.png">
+
+### Your saved article then is placed in the saved article section where you can then delete the article or make notes.
+
+<img width="1639" alt="Readme md image" src="public/assets/images/readme-3.png">
---
+++ server.js
@@ -0,0 +1,44 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser"); //JSON responses
+const mongoose = require("mongoose"); //Mongo object modelling 
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio"); //Scraper
+const mongodb = require('mongodb');
+
+// Require all models
+const db = require("./models");
+
+
+// Port configuration for local/Heroku
+const PORT = process.env.PORT || process.argv[2] || 3000;
+
+// Initialize Express
+const app = express();
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({extended: true }));
+
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// Controllers
+const router = require("./controllers/api.js");
+app.use(router);
+// Connect to the Mongo DB
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
+
+// Start the server
+app.listen(PORT, function () {
+    console.log(`This application is running on port: ${PORT}`);
+   
+});
